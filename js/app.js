df12e2612afd1d9ed92db860db23c4174a903a42
Johan Meneses <johanmeci03@gmail.com>
2022-02-20 18:39:01 -0500

    Adding delete

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function () {
 
     let DB;
+    const clientsList = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         createDB();
@@ -8,6 +9,8 @@
         if (window.indexedDB.open('crm', 1)) {
             loadClientsList();
         }
+
+        clientsList.addEventListener('click', deleteClient);
 
     });
 
@@ -39,8 +42,6 @@
     }
 
     function loadClientsList() {
-
-        console.log('Cargando clientes...');
         
         const connection = window.indexedDB.open('crm', 1);
 
@@ -59,7 +60,7 @@
                 if (cursor) {
 
                     const { name, email, phone, company, id } = cursor.value;
-                    const clientsList = document.querySelector('#listado-clientes');
+
                     clientsList.innerHTML += `
                         <tr>
                             <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
@@ -74,7 +75,7 @@
                             </td>
                             <td class='px-6 py-4 whitespace-no-wrap border-b border-gray-200 leading-5 text-sm leading-5'>
                                 <a class='text-teal-600 hover:text-teal-900 mr-5' href='editar-cliente.html?id=${id}'>Editar</a>
-                                <a class='text-red-600 hover:text-red-900' href='#' data-cliente='${id}'>Eliminar</a>
+                                <a class='text-red-600 hover:text-red-900 btn-delete' href='#' data-cliente='${id}'>Eliminar</a>
                             </td>
                         </tr>
                     `;
@@ -88,6 +89,32 @@
             }
         }
 
+    }
+
+    function deleteClient(e) {
+        
+        if (e.target.classList.contains('btn-delete')) {
+            
+            const id = Number(e.target.dataset.cliente);
+            const confirmDelete = confirm('Are you sure you want to delete the client?');
+
+            if (confirmDelete) {
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(id);
+
+                transaction.oncomplete = function () {
+                    console.log('Eliminado');
+
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function () {
+                    console.log('Error deleting');
+                }
+            }
+        }
     }
 
 })();
